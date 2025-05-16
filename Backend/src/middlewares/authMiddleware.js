4134414a6782c3e8ff4c301f3f324fd860b092ea
shivendra0712 <patelshivendra817@gmail.com>
2025-05-16 18:32:51 +0530

    added authMiddleware.js

---
+++ Backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModels/user.model.js');
+const CustomError = require('../utils/customError.js')
+const cacheClient = require('../services/cache.services.js')
+
+
+const authMiddleware = async (req,res,next)=>{
+    const {token} = req.cookies;
+
+    try{
+        if(!token) return next(new CustomError('Unauthorized user!',401));
+        
+        const isBlacklistToken = await cacheClient.get(token);
+        if(isBlacklistToken){
+            return res.status(401).json({message:"token blacklisted"});
+        }
+
+        const decode = jwt.verify(token ,process.env.Secret_key);
+        console.log('decoded ---->', decode);
+        const user = await User.findById(decode.id);
+
+        if(!user) return next(new CustomError('user not found',401));
+
+        req.user = user;
+        next();
+
+    } catch(error){
+        next(new CustomError(error.message , 500));
+    }
+};
+
+module.exports = authMiddleware;
