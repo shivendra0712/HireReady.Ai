836df1b053269ad92d97e2bf138d6d7d29834b19
shivendra0712 <patelshivendra817@gmail.com>
2025-05-16 18:31:53 +0530

    added user.controller.js file

---
+++ Backend/src/controllers/userControllers/user.controller.js
@@ -0,0 +1,92 @@
+const User = require('../../models/userModels/user.model.js');
+const bcrypt = require('bcryptjs');
+const CustomError = require('../../utils/customError.js');
+const cacheClient = require('../../services/cache.services.js');
+
+const registerController = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existinguser = await User.findOne({ email });
+        if (existinguser) return next(new CustomError('user already exist', 409));
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+
+        const token = await user.generateAuthToken();
+        console.log('token inside constroller --->', token);
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'none',
+        });
+
+        res.status(201).json({ message: 'User created successfull', token: token });
+
+
+    }
+    catch (error) {
+        next(new CustomError('error.message', 500));
+    }
+
+}
+
+
+const loginController = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.authenticateUser(email, password);
+        console.log("user->", user);
+
+        const token = await user.generateAuthToken();
+        console.log("this way token -> ", token);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "None",
+        });
+
+        res.status(200).json({ message: "User Logged in", token: token });
+    } catch (error) {
+        next(new CustomError(error.message, 500));
+    }
+};
+
+
+const logoutController = async (req, res, next) => {
+    const { token } = req.cookies;
+    try {
+        if (!token) return next(new CustomError('User unauthorized', 401));
+
+        const blocklistToken = await cacheClient.set(
+            token,
+            'blocklisted',
+            'EX',
+            3600
+        );
+
+        res.clearCookie('token');
+        res.status(200).json({ message: 'user logged out ' });
+
+    }
+    catch (error) {
+        next(new CustomError(error.message, 500))
+    }
+}
+
+
+const currentUserController = async (req, res, next) => {
+
+    try {
+        const user = req.user;
+        res.status(200).json({ message: 'authentication successfull ', user: user });
+    } catch (error) {
+
+    }
+}
+
+
+module.exports = { registerController, loginController, logoutController, currentUserController };
