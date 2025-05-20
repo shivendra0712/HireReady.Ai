213164cee060100b94d35070f60780296b25a099
shivendra0712 <patelshivendra817@gmail.com>
2025-05-20 10:43:42 +0530

    added file

---
+++ Frontend/src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DashboardMenu from './components/DashboardMenu';
+import DashboardContent from './components/DashboardContent';
+
+
+const Dashboard = () => {
+  return (
+    <DashboardContent />
+  );
+};
+
+export default Dashboard;
+
