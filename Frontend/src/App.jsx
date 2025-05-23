dfbc5664f1533a211bab4cc36d2925b4161a0b20
shivendra0712 <patelshivendra817@gmail.com>
2025-05-23 09:03:51 +0530

    updated file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,37 +1,8 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Feed from './components/Feed'
-import Dashboard from './Dashboard'
-import Interview from './components/Interview'
-import Interviews from './components/Interviews'
-import Join from './components/Join'
-import DashboardMenu from './components/DashboardMenu'
-
-const Layout = () => (
-  <div className="w-screen h-screen flex bg-[#09090B] text-white">
-    <div className="w-[18%] h-full">
-      <DashboardMenu />
-    </div>
-    <div className="w-[82%] h-full p-2">
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-interview" element={<Interview />} />
-        <Route path="/interview/join/:interviewId" element={<Join />} />
-        <Route path="/interview/report/:interviewId" element={<Interview />} />
-         <Route path="/interviews" element={<Interviews />} />
-      </Routes>
-    </div>
-  </div>
-);
+import MainRoutes from "./routes/MainRoutes.jsx";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/*" element={<Layout />} />
-      </Routes>
-    </Router>
+    <MainRoutes/>
   );
 };
 
