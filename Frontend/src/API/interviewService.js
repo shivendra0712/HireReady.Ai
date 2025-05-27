9eca2583c04ef37697b006ea1e293c1c05a55f47
shivendra0712 <patelshivendra817@gmail.com>
2025-05-27 23:08:34 +0530

    changes in service

--- Frontend/src/API/interviewService.js
+++ Frontend/src/API/interviewService.js
@@ -1,51 +1,51 @@
 import axios from './axiosConfig.js'
 
-export const createInterviewService = async (data)=>{
-    try{
+export const createInterviewService = async (data) => {
+    try {
         const res = await axios.post('/interview/create', data);
         return res;
     }
-    catch(error){
-       console.log("Error ----> " , error.response.data.message);
+    catch (error) {
+        console.log("Error ----> ", error.response.data.message);
     }
 }
 
-export const viewAllInterviewService = async ()=>{
-    try{
+export const viewAllInterviewService = async () => {
+    try {
         const res = await axios.get('/interview/view-all');
-        console.log(res);
+         return res;
     }
-    catch(error){
-        console.log("Error ----> " , error.response.data.message);
-    }
-}
-
-export const viewInterviewByIdService = async ()=>{
-    try{
-        const res = await axios.get('/interview/view/:id');
-        console.log(res);
-    }
-    catch(error){
-        console.log("Error ----> " , error.response.data.message);
+    catch (error) {
+        console.log("Error ----> ", error.response.data.message);
     }
 }
 
-export const startInterviewByIdService = async ()=>{
-    try{
-        const res = await axios.put('/interview/start/:id');
-        console.log(res);
+export const viewInterviewByIdService = async (id) => {
+    try {
+        const res = await axios.get(`/interview/view/:${id}`);
+        return res;
     }
-    catch(error){
-        console.log("Error ----> " , error.response.data.message);
+    catch (error) {
+        console.log("Error ----> ", error.response.data.message);
     }
 }
 
-export const endInterviewByIdService = async ()=>{
-    try{
-        const res = await axios.put('/interview/end/:id');
-        console.log(res);
+export const startInterviewByIdService = async (id) => {
+    try {
+        const res = await axios.put(`/interview/start/:${id}`);
+        return res;
     }
-    catch(error){
-        console.log("Error ----> " , error.response.data.message);
+    catch (error) {
+        console.log("Error ----> ", error.response.data.message);
+    }
+}
+
+export const endInterviewByIdService = async () => {
+    try {
+        const res = await axios.put(`/interview/end/:${id}`);
+        return res;
+    }
+    catch (error) {
+        console.log("Error ----> ", error.response.data.message);
     }
 }
--- Frontend/src/API/questionService.js
+++ Frontend/src/API/questionService.js
@@ -3,27 +3,27 @@ import axios from './axiosConfig.js'
 export const createQuestionService = async (data)=>{
     try{
         const res = await axios.post('/question/create', data);
-        console.log(res);
+         return res;
     }
     catch(error){
         console.log("Error ----> " , error.response.data.message);
     }
 }
 
-export const viewQuestionByIdService = async ()=>{
+export const viewQuestionByIdService = async (id)=>{
     try{
-        const res = await axios.get('/question/view/:id');
-        console.log(res);
+        const res = await axios.get(`/question/view/:${id}`);
+         return res;
     }
     catch(error){
         console.log("Error ----> " , error.response.data.message);
     }
 }
 
-export const updateAnswerByIdService = async ()=>{
+export const updateAnswerByIdService = async (id)=>{
     try{
-        const res = await axios.put('/question/update-answer/:id');
-        console.log(res);
+        const res = await axios.put(`/question/update-answer/:${id}`);
+         return res;
     }
     catch(error){
         console.log("Error ----> " , error.response.data.message);
