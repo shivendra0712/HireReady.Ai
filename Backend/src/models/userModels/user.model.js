d3a95d4136e23b6398ba1bd2f3c0a2e0a93967ae
shivendra0712 <patelshivendra817@gmail.com>
2025-06-10 21:53:08 +0530

    updated user model

--- Backend/src/models/userModels/user.model.js
+++ Backend/src/models/userModels/user.model.js
@@ -35,6 +35,10 @@ const userSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Interview'
     }],
+    userPayments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Payment'
+    }],
 
 }, {
     timestamps: true,
