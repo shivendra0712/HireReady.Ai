12408e5c503516945258922a0a6b35569c9aec15
shivendra0712 <patelshivendra817@gmail.com>
2025-06-10 21:53:20 +0530

    added payment model

---
+++ Backend/src/models/paymentModels/payment.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    totalPrice: {
+        type: Number,
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Completed", "Cancelled"],
+        default: "Pending",
+    },
+    razorpayOrderId: { type: String },
+
+    paymentDetails: {
+        payment_id: { type: String },
+        order_id: { type: String },
+        signature: { type: String },
+    },
+   },
+    {
+        timestamps: true,
+    }
+)
+
+const Payment = mongoose.model("Payment", paymentSchema);
+
+module.exports = Payment;
+
