b1704e3ad4c278cde69e42b625a5694418d66559
shivendra0712 <patelshivendra817@gmail.com>
2025-06-10 21:53:35 +0530

    added payment route

---
+++ Backend/src/routes/paymentRoutes/payment.route.js
@@ -0,0 +1,7 @@
+// const express = require('express');
+// const router = express.Router();
+// const  authMiddleware = require('../../middlewares/authMiddleware.js')
+
+
+
+// module.exports = router;
