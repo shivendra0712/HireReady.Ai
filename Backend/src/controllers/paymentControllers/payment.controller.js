6614b430130a1f80f5d251e564bc062c5a2269a7
shivendra0712 <patelshivendra817@gmail.com>
2025-06-19 23:47:13 +0530

    updated controller

--- Backend/src/controllers/paymentControllers/payment.controller.js
+++ Backend/src/controllers/paymentControllers/payment.controller.js
@@ -1,10 +1,13 @@
 const Payment = require('../../models/paymentModels/payment.model.js');
+const instance = require('../../services/payment.service.js');
+const CustomError = require('../../utils/customError.js');
+const crypto = require("crypto");
 
-
-exports.processPaymentController = async (req, res) => {
+exports.processPaymentController = async (req, res, next) => {
   try {
     const { amount } = req.body;
 
+    console.log('amount --->', amount);
     const options = {
       amount: amount * 100, // amount in paise
       currency: "INR",
@@ -20,6 +23,11 @@ exports.processPaymentController = async (req, res) => {
       status: "Pending",
     });
 
+    const user = await req.user;
+
+      user.userPayments.push(payment._id)
+      await user.save();
+
     res.status(200).json({
       success: true,
       orderId: order.id,
@@ -27,17 +35,18 @@ exports.processPaymentController = async (req, res) => {
       key: process.env.RAZORPAY_KEY_ID,
     });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Payment initiation failed" });
+    next(new CustomError('Payment initiation failed', 500));
+    // res.status(500).json({ success: false, message: "Payment initiation failed" });
   }
 };
 
 
-
-exports.verifyPaymentController = async (req, res) => {
+exports.verifyPaymentController = async (req, res, next) => {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    console.log('verify data in backend ----> ', razorpay_order_id, razorpay_payment_id, razorpay_signature)
 
-    const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+    const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
     hmac.update(razorpay_order_id + "|" + razorpay_payment_id);
     const generatedSignature = hmac.digest("hex");
 
@@ -55,12 +64,17 @@ exports.verifyPaymentController = async (req, res) => {
           },
         }
       );
-      return res.status(200).json({ success: true, message: "Payment verified successfully" });
+
+      
+
+      return res.status(200).json({ data: true, message: "Payment verified successfully" });
     } else {
-      return res.status(400).json({ success: false, message: "Invalid signature" });
+      return next(new CustomError('Invalid signature', 400));
+      // return res.status(400).json({ success: false, message: "Invalid signature" });
     }
   } catch (error) {
-    res.status(500).json({ success: false, message: "Payment verification failed" });
+    next(new CustomError('Payment verification failed', 500));
+    // res.status(500).json({ success: false, message: "Payment verification failed" });
   }
 };
 
