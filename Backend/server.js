b770d15ae09abe10f98cde92869252d013c419c1
shivendra0712 <patelshivendra817@gmail.com>
2025-06-20 23:20:29 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv').config();
 const app = require('./src/app.js')
 const connect = require('./src/config/db/db.js')
 
-
 connect();
 
 const port = process.env.PORT || 3000;
