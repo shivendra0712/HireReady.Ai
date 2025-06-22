5e8eb24e9b90886aa36a7fbc2d2e01eed44efbe2
shivendra0712 <patelshivendra817@gmail.com>
2025-06-22 23:57:15 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const userRoutes = require('./routes/userRoutes/user.route.js');
 const questionRoutes = require('./routes/questionRoutes/question.route.js')
 const interviewRoutes = require('./routes/interviewRoutes/interview.route.js')
+const paymentRoutes = require('./routes/paymentRoutes/payment.route.js')
 
 
 const cookieParser = require('cookie-parser');
@@ -23,7 +24,6 @@ app.use(cookieParser());
 app.use(morgan('tiny'));
 
 
-
 app.get('/',(req,res)=>{
     res.send('hello from server');
 })
@@ -31,6 +31,7 @@ app.get('/',(req,res)=>{
 app.use('/api/auth',userRoutes);
 app.use('/api/question',questionRoutes);
 app.use('/api/interview',interviewRoutes);
+app.use('/api/payment',paymentRoutes);
 
 app.use(errorHandler);
 
