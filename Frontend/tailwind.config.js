da45ba5e2e6bac8831cbcd32efae658c53c3d744
shivendra0712 <patelshivendra817@gmail.com>
2025-06-27 15:08:53 +0530

    file

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+import scrollbar from 'tailwind-scrollbar';
+
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [scrollbar],
+}
