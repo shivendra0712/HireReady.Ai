e5958a72d2a4f05ea25746dbfe36d3a7e15ad08a
shivendra0712 <patelshivendra817@gmail.com>
2025-11-13 14:18:08 +0530

    updated file and added new interview video2

--- Frontend/src/pages/Section5Right.jsx
+++ Frontend/src/pages/Section5Right.jsx
@@ -4,9 +4,7 @@ const Section5Right = () => {
     return (
         <div>
             <div className="w-full h-full  px-4 md:px-6 lg:px-0 lg:scale-110">
-                <video autoplay loop 
-                    muted className='w-full' src="/images/interview1.mp4">
-
+                <video autoPlay loop muted controls  className='w-full' src="/images/interview2.mp4">
                 </video>
             </div>
         </div>
--- Frontend/src/pages/SectionRight.jsx
+++ Frontend/src/pages/SectionRight.jsx
@@ -4,8 +4,7 @@ const SectionRight = memo(() => {
     return (
         <div>
             <div className="w-full h-full  px-4 md:px-6 lg:px-0 lg:scale-110">
-                <video autoplay loop 
-                    muted className='w-full' src="/images/interview1.mp4">
+                <video autoPlay loop muted controls className='w-full' src="/images/interview1.mp4">
                 </video>
             </div>
         </div>
